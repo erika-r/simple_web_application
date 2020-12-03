51b0b9071703a1f6120de73aad1d0d5cbfeb84c5
Erika Rellermo <56734635+erika-r@users.noreply.github.com>
2020-12-03 16:10:16 +0000

    added redirection

--- src_code/web_app.py
+++ src_code/web_app.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template
+#. venv/bin/activate
+
+from flask import Flask, render_template,flash,url_for,redirect,request,session
+from forms import *
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "a-key-that-is-secret"
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html',title="Web App - Home")
+
+@app.route("/formtest",methods=["GET","POST"])
+def formtest():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # flash(f"Name entered: {form.username.data}")
+        username = form.username.data
+        session["username"] = username
+        return redirect(url_for("name",username=username))
+    return render_template('formtest.html',title="Web App - Form",form=form)
+
+@app.route("/name")
+def name():
+    username = request.args["username"]
+    username = session["username"]
+    return render_template('name.html',title="Web App - Name",username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
