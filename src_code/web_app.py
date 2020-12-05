55380b4c8e77bd1500a1ec8257d167242cdb347c
Erika Rellermo <56734635+erika-r@users.noreply.github.com>
2020-12-05 20:03:40 +0000

    setting up for allegiance file testing

--- src_code/web_app.py
+++ src_code/web_app.py
@@ -1,7 +1,9 @@
 #. venv/bin/activate
 
-from flask import Flask, render_template,flash,url_for,redirect,request,session
+from flask import Flask, render_template,flash,url_for,redirect,request,session,jsonify
 from forms import *
+import pandas as pd
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "a-key-that-is-secret"
 
@@ -25,6 +27,15 @@ def name():
     username = request.args["username"]
     username = session["username"]
     return render_template('name.html',title="Web App - Name",username=username)
+
+@app.route("/allegiances")
+def allegiances():
+    json_ = request.json
+    allegiance = pd.read_csv('allegiance.csv')
+    json_vector = allegiance.transform(json_)
+    query = pd.DataFrame(json_vector)
+    prediction = regr.predict(query)
+    return json.dumps({'allegiances': list({{allegiances}})})    
 
 if __name__ == '__main__':
     app.run(debug=True)
