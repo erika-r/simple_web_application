d21d4ff6a0d2be12f637d17d7c4ab56ebbeb5ff2
Erika Rellermo <56734635+erika-r@users.noreply.github.com>
2020-12-09 14:04:43 +0000

    added jquery section

--- src_code/web_app.py
+++ src_code/web_app.py
@@ -1,8 +1,8 @@
 #. venv/bin/activate
 
-from flask import Flask, render_template,flash,url_for,redirect,request,session,jsonify,Response
+from flask import Flask, render_template,url_for,redirect,request,session,jsonify,Response
 from forms import *
-import json
+import json,requests
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "a-key-that-is-secret"
@@ -17,23 +17,28 @@ def home():
 def formtest():
     form = LoginForm()
     if form.validate_on_submit():
-        # flash(f"Name entered: {form.username.data}")
         username = form.username.data
-        session["username"] = username
+        # session["username"] = username
         return redirect(url_for("name",username=username))
     return render_template('formtest.html',title="Web App - Form",form=form)
 
 @app.route("/name")
 def name():
     username = request.args["username"]
-    username = session["username"]
+    # username = session["username"]
     return render_template('name.html',title="Web App - Name",username=username)
 
-@app.route("/allegiances")
+@app.route("/allegiances",methods=["GET","POST"])
 def allegiances():
+
     #handle csv file
     with open("allegiance.json","r") as jsonFile:
-        return Response(jsonFile.read(),mimetype="text/json")
+        data = json.load(jsonFile)
+        return jsonify(data)     #Response(jsonFile.read(),mimetype="text/json")
+
+@app.route("/allegiancedashboard")
+def allegiancedashboard():
+    return render_template("allegiancedashboard.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
